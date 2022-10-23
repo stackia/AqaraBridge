c4e95507ddba30770a06311a6697d47eee16e2ca
meishild <meishild@gmail.com>
2022-10-23 19:10:43 +0800

    fix tzlocal error

--- custom_components/aqara_bridge/core/utils.py
+++ custom_components/aqara_bridge/core/utils.py
@@ -1,5 +1,4 @@
 """ the Aqara Bridge utils."""
-import tzlocal
 from datetime import datetime
 
 from homeassistant.core import HomeAssistant
@@ -9,7 +8,7 @@ def local_zone(hass=None):
     try:
         if isinstance(hass, HomeAssistant):
             return get_time_zone(hass.config.time_zone)
-        return tzlocal.get_localzone()
+        return DEFAULT_TIME_ZONE
     except KeyError:
         pass
     return DEFAULT_TIME_ZONE
